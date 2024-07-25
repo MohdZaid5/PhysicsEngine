1a6654d2ecdc8a65b800be15bbd9e80169d3f2c4
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2024-07-25 17:44:06 +0530

    vectors working properly

--- makefile
+++ makefile
@@ -5,7 +5,7 @@ prepare:
 	mkdir -p Bin
 
 program: program.c
-	gcc program.c -o Bin\program.exe
+	gcc program.c Bin\vectors.o -o Bin\program.exe -IInclude -ISource
 
 vectors: Source\vectors.c
 	gcc -c Source\vectors.c -o Bin\vectors.o
--- program.c
+++ program.c
@@ -1,6 +1,14 @@
 
 #include <stdio.h>
+#include "Include\vectors.h"
 
 int main(){
-    printf("Hello world");
+    
+    Vector2 Origin = {
+        .x = 1,
+        .y = 2
+    };
+
+    printf( "This vector has x:%f and y:%f", Origin.x, Origin.y );
+
 }
