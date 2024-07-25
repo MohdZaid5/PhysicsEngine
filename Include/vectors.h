ae488504f02dd7aad9606b8be7cb859641f25d82
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2024-07-25 17:05:08 +0530

    defined struct `Vector2`

---
+++ Include/vectors.h
@@ -0,0 +1,21 @@
+#ifndef VECTORS
+#define VECTORS
+
+#ifndef RAYLIB_H
+
+    /**
+     * @brief Represents a 2D vector or point.
+     *
+     * The `Vector2` structure is used to store the x and y coordinates of a vector or point in 2D space.
+     * This structure is commonly used in various mathematical and graphical computations such as
+     * vector operations, geometric transformations, and graphical representations.
+     */
+    typedef struct Vector2 {
+        float x; // The x-coordinate of the vector or point.
+        float y; // The y-coordinate of the vector or point.
+    } Vector2;
+
+#endif
+
+
+#endif
